4c0296d37e871c7b5227205920922afde3bf7f3d
Kelly Rocha Hayd <krocha-h@c1r2p1.42sp.org.br>
2023-11-13 15:07:42 -0300

    remodelagem

--- get_next_line.c
+++ get_next_line.c
@@ -6,22 +6,43 @@
 /*   By: krocha-h <krocha-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 12:54:46 by krocha-h          #+#    #+#             */
-/*   Updated: 2023/11/10 15:33:49 by krocha-h         ###   ########.fr       */
+/*   Updated: 2023/11/13 14:05:39 by krocha-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdlib.h>
 
 char	*get_next_line(int fd)
 {
+	int		i;
 	char	c;
-	char	*line;
-	char	*tmp;
+	char	buffer[BUFFER_SIZE];
 
-	open(fd, O_RDONLY);
-	while (c != '\n')
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	i = 0;
+	while (buffer[i] != '\n')
 	{
-		read()
+		while (read(fd, &c, 1) != '\n')
+		{
+			if (!c)
+				return (0);
+			buffer[i] = c;
+			i++;
+		}
+		i++;
 	}
+	return (buffer);
 }
+
+int	read_line;
+char	*buffer;
+
+buffer = malloc(BUFFER_SIZE + 1 * sizeof(char));
+if (!buffer)
+	return (0);
+read_line = read(fd, buffer, BUFFER_SIZE);
+if (read_line < 1)
+	return (0);
+buffer[read_line] = '\0';
+
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,19 @@
 /*   By: krocha-h <krocha-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 12:55:14 by krocha-h          #+#    #+#             */
-/*   Updated: 2023/11/10 15:32:21 by krocha-h         ###   ########.fr       */
+/*   Updated: 2023/11/13 12:35:06 by krocha-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+# endif
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdlib.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
 
