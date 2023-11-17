c041fb7c6cd51c556529a6b8412d6693d7f2bf7d
Kelly Rocha Hayd <krocha-h@c2r5p2.42sp.org.br>
2023-11-17 12:37:01 -0300

    main: tests

---
+++ main.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: krocha-h <krocha-h@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/17 11:53:04 by krocha-h          #+#    #+#             */
+/*   Updated: 2023/11/17 12:36:39 by krocha-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include "get_next_line.h"
+
+int main () {
+	int	fd;
+
+	fd = open("text_ex", O_RDONLY);
+	printf("Primeira linha: %s", get_next_line(fd));
+	printf("Segunda linha: %s", get_next_line(fd));
+	printf("Terceira linha: %s", get_next_line(fd));
+	printf("Quarta linha: %s", get_next_line(fd));
+	printf("Quinta linha: %s", get_next_line(fd));
+	printf("Sexta linha: %s", get_next_line(fd));
+	printf("Sem mais linhas: %s\n", get_next_line(fd));
+	close (fd);
+
+	fd = open("text_ex", O_RDONLY);
+	printf("Arquivo invállido: %s\n", get_next_line(9));
+	printf("%s\n", get_next_line(fd));
+	close (fd);
+
+	fd = open("1char.txt", O_RDONLY);
+	printf("Arquivo invállido:%s\n", get_next_line(9));
+	printf("%s", get_next_line(fd));
+	printf("Arquivo lido: %s\n", get_next_line(fd));
+	close (fd);
+
+	return (0);
+}
