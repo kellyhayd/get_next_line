e1b8af227e2b691389cc05b9ca89183e25c033c0
Kelly Hayd <69431689+kellyhayd@users.noreply.github.com>
2023-11-22 12:39:51 -0300

    reorganization

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,15 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: haydkelly <haydkelly@student.42.fr>        +#+  +:+       +#+        */
+/*   By: krocha-h <krocha-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 12:55:04 by krocha-h          #+#    #+#             */
-/*   Updated: 2023/11/21 19:46:41 by haydkelly        ###   ########.fr       */
+/*   Updated: 2023/11/22 09:50:34 by krocha-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdlib.h>
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (lst == NULL)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
 
 int	has_nl(t_list *lst)
 {
@@ -27,41 +36,21 @@ int	has_nl(t_list *lst)
 	return (0);
 }
 
-t_list  *ft_lstlast(t_list *lst)
+int	lstadd_node(t_list **lst, t_list **last_node, char *buffer)
 {
-    if (lst == NULL)
-        return (NULL);
-    while (lst->next)
-        lst = lst->next;
-    return(lst);
-}
+	t_list	*new_node;
 
-void	create_str(t_list *lst, char *line)
-{
-	size_t	i;
-	size_t	j;
-
-	if (!lst)
-		return ;
-	i = 0;
-	while (lst)
-	{
-		j = 0;
-		while (lst->content[j])
-		{
-			if (lst->content[j] == '\n')
-			{
-				line[i] = '\n';
-				line[i + 1] = '\0';
-				return ;
-			}
-			line[i] = lst->content[j];
-			j++;
-			i++;
-		}
-		lst = lst->next;	
-	}
-	line[i] = '\0';
+	new_node = malloc(sizeof(t_list));
+	if (!new_node)
+		return (0);
+	if (!*last_node)
+		*lst = new_node;
+	else
+		(*last_node)->next = new_node;
+	new_node->content = buffer;
+	new_node->next = NULL;
+	(*last_node) = new_node;
+	return (1);
 }
 
 size_t	get_line_len(t_list *lst)
@@ -84,7 +73,7 @@ size_t	get_line_len(t_list *lst)
 			len++;
 		}
 		lst = lst->next;
-	}	
+	}
 	return (len);
 }
 
